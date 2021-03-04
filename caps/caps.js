7cee4063b51efa88bf4089e8061921247c39491d
seido <seid783@gmail.com>
2021-03-03 19:06:57 -0800

    message queue

--- caps/caps.js
+++ caps/caps.js
@@ -6,13 +6,31 @@ const events = require('../event.js');
 const io = socket_io(3001);
 const caps = io.of('/caps');
 
+
+const orderQueue = {
+  sent: {
+
+  },
+  recieved:{
+
+  }
+  
+};
+
 caps.on('connection', (socket) => {
   console.log('new connection created', socket.id);
+
+  socket.on('packageReady', payload => {
+    orderQueue.sent[payload.orderID] = payload;
+
+    caps.emit('packageReady', payload)
+})
 
   socket.on('pickup', payload => {
     events.eventPickup(payload);
     caps.emit('pickup', payload);
   })
+
   
   socket.on('in-transit', payload => {
     events.eventIntransit(payload);
@@ -22,6 +40,11 @@ caps.on('connection', (socket) => {
   socket.on('delivered', payload => {
     events.eventDelivered(payload);
     caps.emit('delivered', payload);
+  });
+
+  socket.on('delivered', payload => {
+  
+    events.vendorDelivered(payload);
   });
 
 })
--- caps/queue-server.js
+++ caps/queue-server.js
@@ -4,7 +4,7 @@ const socket_io = require('socket.io');
 
 const io = socket_io(3001);
 
-const messageQueue = {
+const orderQueue = {
     sent: {
 
     },
@@ -12,6 +12,25 @@ const messageQueue = {
 
     },
     recieved:{
-        
+
     }
-}
+    
+};
+
+io.on('connection', (socket) =>{
+    socket.on('packageReady', payload => {
+        orderQueue.sent[payload.orderID] = payload;
+
+        socket.broadcast.emit('packageReady', payload)
+    })
+    socket.on('recieved', payload =>{
+        delete orderQueue.sent[payload.orderID];
+        orderQueue.recieved[payload.orderID] = payload;
+    })
+    socket.on('getMessages', () => {
+        for (let key in orderQueue.sent) {
+    
+          socket.emit('packageReady', orderQueue.sent[key]);
+        }
+      });
+})
